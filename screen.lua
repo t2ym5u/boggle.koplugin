5f96435f2df85f0f478ca110bcf551a6e0b5ff5b
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-08 14:02:29 +0200

    feat: i18n FR/EN translation + bump to 1.1.0

--- screen.lua
+++ screen.lua
@@ -18,7 +18,7 @@ local TextWidget      = require("ui/widget/textwidget")
 local UIManager       = require("ui/uimanager")
 local VerticalGroup   = require("ui/widget/verticalgroup")
 local VerticalSpan    = require("ui/widget/verticalspan")
-local _               = require("gettext")
+local _               = require("i18n")
 local T               = require("ffi/util").template
 
 local Font       = require("ui/font")
@@ -152,18 +152,13 @@ function BoggleScreen:buildLayout()
             right,
         }
     else
-        self.layout = VerticalGroup:new{
+        local content = VerticalGroup:new{
             align = "center",
-            VerticalSpan:new{ width = Size.span.vertical_large },
-            top_buttons,
-            VerticalSpan:new{ width = Size.span.vertical_large },
             board_frame,
             VerticalSpan:new{ width = Size.span.vertical_large },
             self.status_text,
-            VerticalSpan:new{ width = Size.span.vertical_large },
-            self.word_list_widget,
-            VerticalSpan:new{ width = Size.span.vertical_large },
         }
+        self:buildPortraitLayout(top_buttons, content, self.word_list_widget)
     end
     self[1] = self.layout
     self:updateStatus()
