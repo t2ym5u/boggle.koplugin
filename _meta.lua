d7a3c6c8c51c361d791757d0004037873c93fcdc
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-15 14:19:20 +0200

    chore: bump version to 1.1.5

--- _meta.lua
+++ _meta.lua
@@ -4,5 +4,5 @@ return {
     name        = "boggle",
     fullname    = _("Boggle"),
     description = _("Find words by connecting adjacent letters on the grid."),
-    version     = "1.1.4",
+    version     = "1.1.5",
 }
